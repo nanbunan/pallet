da9d8c34f9b78bd4dcc0197ee9f9294572c2c6c3
Mr.G <734620299@qq.com>
2017-09-14 11:55:47 +0800

    commit

---
+++ app/src/main/java/com/example/administrator/pallet/TranslateFragment.java
@@ -0,0 +1,27 @@
+package com.example.administrator.pallet;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2017/9/13.
+ */
+
+public class TranslateFragment extends Fragment {
+    @Override
+    @Nullable
+    public View onCreateView(LayoutInflater inflater,@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        Bundle bundle = getArguments();
+        int layoutId = bundle.getInt("layoutId");
+        int pageIndex = bundle.getInt("pageIndex");
+
+        View view = inflater.inflate(layoutId, null);
+        view.setTag(pageIndex);
+        return view;
+    }
+}
